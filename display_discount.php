2d1b10e197c28e3266e277076ec01214e87b7f08
mgty <marygracety@gmail.com>
2020-02-10 11:15:24 -0500

    set variables in php

---
+++ display_discount.php
@@ -0,0 +1,15 @@
+<?php
+
+//get data from HTML file
+$product_description = $_POST['product_description'};
+$list_price = $_POST['list_price'];
+$discount_percent = $_POST['discount_percent'];
+
+
+
+//calculate the discount
+
+
+//apply currency format with dollar sign
+
+?>
